670d82f66605102901a0c97c4dcfdca2f7e58faf
divinomarciok <58217165+divinomarciok@users.noreply.github.com>
2024-07-27 03:32:42 -0300

    fluxo de busca no banco e tratando antes de enviar

--- index.js
+++ index.js
@@ -36,6 +36,32 @@ async function getChats() {
 }
 
 
+async function consultaProduto() {
+
+    const myHeaders = new Headers();
+    myHeaders.append("x-api-key", "cursodev1");
+
+    const formdata = new FormData();
+
+    const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow"
+    };
+
+    try {
+
+        const response = await fetch("http://localhost:4000/notifica/consulta", requestOptions);
+        const result = await response.json();
+
+        //console.log(result)
+        return result;
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 async function trataRetorno() {
 
     const id_naolidos = [];
@@ -59,40 +85,58 @@ async function trataRetorno() {
     return id_naolidos
 }
 
-async function trataretornoNew() {
 
+
+
+
+async function trataRetornoProduto() {
     try {
 
-        const raw = await consultarchats()
-        const chats = raw.chats;
-        const id_naolidos = []
+        const raw = await consultaProduto()
+        const dados = raw.json;
+        const produtoSelecionado = [];
 
-        chats.forEach(chat => {
-            if (chat.hasOwnProperty('unreadCount')) {
 
-                if (chat.unreadCount != 0) {
-                    const atendimento_unreadCount = {
-                        unreadCount: chat.unreadCount,
-                        name: chat.name,
-                        chatId: chat.id._serialized,
-                        body: chat.lastMessage._data.body
+        for (let i = 0; i < dados.length; i++) {
+            const objeto = dados[i];
+
+
+            const idObjeto = objeto._id;
+            const dataFim = objeto.dataFim;
+            const dataInicio = objeto.dataInicio;
+            const loja = objeto.loja;
+
+            //console.log(`ID: ${idObjeto}, Data Início: ${dataInicio}, Loja: ${loja}`);
+
+            // Iterando sobre os produtos dentro do objeto
+            for (let j = 0; j < objeto.produtos.length; j++) {
+                const produto = objeto.produtos[j];
+                const nomeProduto = produto.nome;
+                const marcaProduto = produto.marca;
+                const precoProduto = produto.preco;
+
+                if (marcaProduto == "Seara") {
+                    const objetoVariavel = {
+                         dataFim : objeto.dataFim,
+                         dataInicio : objeto.dataInicio,
+                         loja: objeto.loja,
+                          nomeProduto :produto.nome,
+                          marcaProduto : produto.marca,
+                          precoProduto : precoProduto.replace(',',':')
                     }
-                    id_naolidos.push(atendimento_unreadCount)
-                } else {
-
+                    produtoSelecionado.push(objetoVariavel)
                 }
-            };
-        })
-        console.log(JSON.stringify(id_naolidos[0], null, 2));
-        console.log(id_naolidos.length)
-        return id_naolidos;
+               // console.log(`  - Produto: ${nomeProduto}, Marca: ${marcaProduto}, Preço: ${precoProduto}`);
+            }
+        }
+        //console.log(produtoSelecionado);
+        return produtoSelecionado;
     } catch (error) {
         console.log(error)
     }
-
 }
 
-async function responde(chatId,resposta) {
+async function responde(chatId, resposta) {
 
     const myHeaders = new Headers();
     myHeaders.append("x-api-key", "cursodev1");
@@ -101,7 +145,7 @@ async function responde(chatId,resposta) {
     const raw = JSON.stringify({
         "chatId": chatId,
         "contentType": "string",
-        "content":resposta
+        "content": resposta
     });
 
     const requestOptions = {
@@ -119,26 +163,31 @@ async function responde(chatId,resposta) {
 }
 
 
+async function transformaresposta(){
+    const resposta = await trataRetornoProduto
+}
+
+
 async function respondeNaolidos() {
     try {
 
         const naolidos = await trataretornoNew()
         //console.log(naolidos)
-        
-        if(naolidos != null){
 
-        naolidos.forEach (chat =>{
-            const idChat = chat.chatId;
-            const body = chat.body;
+        if (naolidos != null) {
 
-            let arrayBody = body.split(",");
+            naolidos.forEach(chat => {
+                const idChat = chat.chatId;
+                const body = chat.body;
 
-            console.log(arrayBody)
-            
-            responde(idChat,arrayBody[0]);
-            
-        })
-    }
+                let arrayBody = body.split(",");
+
+                let resposta = arrayBody[1] + "\n" + arrayBody[0] + "\n" + arrayBody[2]
+
+                responde(idChat, resposta);
+
+            })
+        }
         //responde(" VINHO CAMPO LARGO TINTO SUAV\n\nprice: 9,99\nvolume: 1L\nunit: LITRO\nmarca: CAMPO LARGO")
     } catch (error) {
 
@@ -150,7 +199,20 @@ async function respondeNaolidos() {
 
 //setInterval(trataretornoNew,6000)
 
-setInterval(respondeNaolidos,6000)
+//setInterval(respondeNaolidos, 6000)
+
+  trataRetornoProduto()
+  .then(result =>{
+    let retorno = JSON.stringify(result);
+    let identandoRetorno = retorno.replace(/,/g,'\n') 
+    console.log(identandoRetorno)
+   //responde("556492112609@c.us",retorno)
+  })
+  .catch(error=>{
+    console.error(error)
+  })
+
+
 
 
 //respondeNaolidos()
@@ -158,10 +220,3 @@ setInterval(respondeNaolidos,6000)
 //getChats()
 
 
-/*let receba = "abacaxi, coca-cola, carne, peixe";
-
-let arrayItens = receba.split(",");
-
-let  item1 = arrayItens[3];
-
-console.log(arrayItens)*/
